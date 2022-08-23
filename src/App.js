ff0adb20071b7d6c9ea12f11f7c63b0babad016b
Naru-hub <1208ny0324@gmail.com>
2022-08-23 23:52:32 +0900

    Main componentの追加 react-beautiful-dndを使ってtitleの表示

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Main from "./components/Main";
 import "./App.css";
 
 function App() {
   return (
     <div style={{ padding: "50px" }}>
       <h1 style={{ marginBottom: "20px" }}>Trello風アプリ</h1>
+      <Main />
     </div>
   );
 }
